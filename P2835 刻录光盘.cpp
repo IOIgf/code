9169a4d2306a8cff1353ada0bb68b88e4355abe2
IOIgf <zbt662720@outlook.com>
2026-02-25 18:24:56 +0800

    1

--- P2835 刻录光盘.cpp
+++ P2835 刻录光盘.cpp
@@ -1,26 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m;
-int fa[100005];
-void add(int x,int y){
-	fa[x]=y;
+int n;
+const int N=1e5+5;
+vector<int> g[N];
+int dfn[N],low[N],w[N],idx;
+int ins[N],cnt;
+stack<int> stk;
+int rd[N];
+void tarjan(int u){
+    dfn[u]=low[u]=++idx;
+    ins[u]=1;
+    stk.push(u);
+    for(auto v:g[u]){
+        if(!dfn[v]){
+            tarjan(v);
+            low[u]=min(low[u],low[v]);
+        }
+        if(ins[v])low[u]=min(low[u],dfn[v]);
+    }
+    if(dfn[u]==low[u]){
+        ++cnt;
+        while(1){
+            int v=stk.top();
+            stk.pop();
+            w[v]=cnt;
+            ins[v]=0;
+            if(v==u)break;
+        }
+    }
 }
 int main(){
     cin>>n;
-    int cnt=0;
-    for(int i=1;i<=n;i++)fa[i]=i;
     for(int i=1;i<=n;i++){
         int u;
         while(1){
             cin>>u;
-            if(u==0)break;
-            add(u,i);
+            if(!u)break;
+            g[i].push_back(u);
         }
     }
-	for(int i=1;i<=n;i++){
-        if(fa[i]==i)cnt++;
+    for(int i=1;i<=n;i++){
+        if(!dfn[i]){
+            tarjan(i);
+        }
     }
-    if(cnt==0)cout<<cnt+1<<endl;
-    else cout<<cnt<<endl;
-	return 0;
+    for(int i=1;i<=n;i++){
+        for(auto v:g[i]){
+            if(w[i]!=w[v])rd[w[v]]++;
+        }
+    }
+    int ans=0;
+    for(int i=1;i<=cnt;i++){
+        if(rd[i]==0)ans++;
+    }
+    cout<<ans;
+    return 0;
 }
